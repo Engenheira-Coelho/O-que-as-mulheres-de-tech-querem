a1aa9fac5465332e91f1fad8cf3f0907de7bba33
engenheiracoelho <leticia.ac23@gmail.com>
2022-10-06 23:15:49 -0300

    feat: Create the initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './general/style/App.css';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
