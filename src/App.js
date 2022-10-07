55f5418d5c85601407798905be5730324ed35c91
Marcelle Vargas da Silva <marcelle.vargas@arctouch.com>
2022-10-07 12:16:15 -0300

    (en) - Footer basic structure (pt) - Estrutura básica do footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './general/style/App.css';
 import Header from './components/header/header';
-
+import Footer from './components/footer/footer';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
