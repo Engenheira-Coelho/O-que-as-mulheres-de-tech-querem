73f2ddc80c8ecd541317e388ae0c59302fb10b6c
arctouch-leticiacoelho <leticia.coelho@arctouch.com>
2022-10-10 20:13:07 -0300

    FEAT: Add new CSS to the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
 
   render() {
     return(
-      <header className="container">
+      <header className="header">
         <p>
           Hello Woman!
         </p>
