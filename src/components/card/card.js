f00b3a3017d3d5e1445a6b41fe9edf49a3b2f3da
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-25 09:56:55 -0300

    Fix: Fix accessibility on card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./card.css";
 
 function Card(props) {
-  console.log(props)
+
   return (
     <div
       className="card"
       style={{
         backgroundImage: `url(${props.image})`,
         backgroundColor: `${props.color}`,
-        
-      }}
-      alt={props.image_description}
+      }} 
+      title={props.image_description}
     >
       <p className="description">{props.description}</p>
     </div>
