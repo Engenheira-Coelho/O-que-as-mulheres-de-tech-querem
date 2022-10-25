4c21c00898a48259bb71c7a4488a300f99178c4d
engenheiracoelho <leticia.ac23@gmail.com>
2022-10-24 21:21:01 -0300

    FEAT: Add map to card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './general/style/App.css';
 import Header from './components/header/header';
 import Card from './components/card/card';
+import SITUATIONS from './general/situations'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Card
-        imageUrl="https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60"
-        comment="A woman's opinion needs to be respected like a man's."
-      />
+      {SITUATIONS.map((item) => (
+        <Card
+          description={item.description}
+          image={item.image}
+          image_description={item.image_description}
+          color={item.color} />
+      ))}
     </div>
   );
 }
