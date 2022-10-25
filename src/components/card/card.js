1ec5e52bdfcf524416fcb848051629be005b1c80
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-25 14:47:02 -0300

    Feat: Implement card hover with styled component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,19 +1,41 @@
 import React from "react";
-import "./card.css";
+import styled from "styled-components";
 
 function Card(props) {
+  const Card = styled.div`
+    .description {
+      display: none;
+    }
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    word-wrap: break-word;
+    width: 250px;
+    height: 250px;
+    max-width: 250px;
+    max-height: 250px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    text-align: center;
+    background-image: url(${props.image});
+
+    &:hover .description {
+      display: flex;
+      color: black;
+      width: 250px;
+      height: 250px;
+      background-color: ${props.color};
+      font-weight: bold;
+      align-items: inherit;
+      justify-content: center;
+    }
+  `;
 
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: `url(${props.image})`,
-        backgroundColor: `${props.color}`,
-      }} 
-      title={props.image_description}
-    >
+    <Card>
       <p className="description">{props.description}</p>
-    </div>
+    </Card>
   );
 }
 export default Card;
