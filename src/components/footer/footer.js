d4e21b49d6a80021ba339b95b930246ce6025ffe
andreiammoraes <profandreiamoraes@gmail.com>
2022-10-26 21:06:11 -0300

    FEAT: criando estrutura do footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './footer.css';
+import { MdKeyboardArrowUp } from "react-icons/md";
 
-export default class Footer extends Component {
+function Footer (props){
+  console.log(props)
+  return (
+    <footer className="footer">
+      <div className="btn-container">
+        <button className="btn">{props.buttonText}</button>
+        <MdKeyboardArrowUp className="btn-icon"/>
+      </div>
 
-  render() {
-    return(
-      <header className="container">
-        <p>
-          Hello! I'm a footer.
-        </p>
-      </header>
-    )
-  }
+      <div className="description-container">
+        <strong className="title-description">{props.title}</strong>
+        <p className="description">{props.description}</p>
+
+        <a className="text-github" href={props.githubLink}>{props.textGithub}</a>
+
+      </div>
+
+      <div className="project-name">
+        <strong>{props.projectName}</strong>
+      </div>
+    </footer>
+  );
 }
+export default Footer;
