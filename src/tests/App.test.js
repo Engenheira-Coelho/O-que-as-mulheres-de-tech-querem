808e9ff2752572a9f07333ed4398fe6b8f0bad75
Leticia Santanna <leticiafsn_@hotmail.com>
2022-10-30 22:47:09 -0300

    #feat - improved tests.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App page", () => {
+  it("should find a word in page", () => {
+    render(<App />);
+
+    const linkElement = screen.getByText(/hello woman/i);
+    expect(linkElement).toBeTruthy();
+  });
 });
